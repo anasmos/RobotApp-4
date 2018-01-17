ed2fdd73f69952e888f8a7a8487e75877772a050
anasmos <anasmos@gmail.com>
2018-01-17 21:57:01 +0100

    modified if statment in robot.js

--- robot.js
+++ robot.js
@@ -96,16 +96,28 @@
 
     switch (robot.dir) {
       case 'up':
-        y = y < board.length - 1 ? y + 1 : y;
+       // y = y < board.length - 1 ? y + 1 : y;
+        if (y < board.length - 1) {
+          y = y + 1;
+        }
         break;
       case 'down':
-        y = y > 0 ? y - 1 : y;
+       // y = y > 0 ? y - 1 : y;
+        if (y > 0) {
+          y = y - 1;
+       } 
         break;
       case 'left':
-        x = x > 0 ? x - 1 : x;
+        //x = x > 0 ? x - 1 : x;
+        if (x > 0) {
+          x = x - 1;
+        }
         break;
       case 'right':
-        x = x < board[0].length - 1 ? x + 1 : x;
+        //x = x < board[0].length - 1 ? x + 1 : x;
+        if (x < board[0].length - 1) {
+          x = x + 1;
+        }
         break;
     }
 
@@ -142,16 +154,40 @@
 
     switch (robot.dir) {
       case 'up':
-        robot.dir = turnDirection === 'left' ? 'left' : 'right';
+        //robot.dir = turnDirection === 'left' ? 'left' : 'right';
+        if (turnDirection === 'left') {
+          robot.dir = 'left';
+        }
+        else {
+          robot.dir = 'right';
+        }
         break;
       case 'down':
-        robot.dir = turnDirection === 'left' ? 'right' : 'left';
+        //(robot.dir = turnDirection === 'left' ? 'right' : 'left';
+        if (turnDirection === 'left') {
+          robot.dir = 'right';
+        }
+        else {
+          robot.dir = 'left';
+        }
         break;
       case 'left':
-        robot.dir = turnDirection === 'left' ? 'down' : 'up';
+        //robot.dir = turnDirection === 'left' ? 'down' : 'up';
+        if (turnDirection === 'left') {
+          robot.dir = 'down';
+        }
+        else {
+          robot.dir = 'up';
+        }
         break;
       case 'right':
-        robot.dir = turnDirection === 'left' ? 'up' : 'down';
+        //robot.dir = turnDirection === 'left' ? 'up' : 'down';
+        if (turnDirection === 'left') {
+          robot.dir = 'up';
+        }
+        else {
+          robot.dir = 'down';
+        }
         break;
     }
 
